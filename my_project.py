758ee33cb315bd3247be475295454517b04bd1bd
Mustaf_Mohamed <142760189+MustafaMohamed74@users.noreply.github.com>
2025-09-20 17:45:58 +0300

    add the too low logic to my project

--- my_project.py
+++ my_project.py
@@ -4,6 +4,7 @@ num = random.randint(1, 100)
 user = int(input("Guess a number between 1 and 100: "))
 if user == num: 
     print("You guessed it right!")
-
-    
-    
+elif user < num: 
+    print("Too low!")
+else: 
+    print("Too high!")
