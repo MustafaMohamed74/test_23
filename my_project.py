1051c4b7ca53739ee5308857b29a4fec980d3f1f
Mustaf_Mohamed <142760189+MustafaMohamed74@users.noreply.github.com>
2025-09-20 17:00:07 +0300

    remove the too small and too big logic

--- my_project.py
+++ my_project.py
@@ -4,7 +4,6 @@ num = random.randint(1, 100)
 user = int(input("Guess a number between 1 and 100: "))
 if user == num: 
     print("You guessed it right!")
-elif user < num:
-    print("Too low!")
-else:
-    print("Too high!")
+
+    
+    
